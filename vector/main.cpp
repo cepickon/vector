63d877856a3c974af0ed6a528a95389ac4246ae3
Jonáš <jonas.hlinak16@gmail.com>
2026-06-16 09:22:06 +0200

    Update main.cpp

--- vector/main.cpp
+++ vector/main.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include "OwnVector.h"
 
 int main(){
@@ -10,8 +10,15 @@ int main(){
 	ownV.pushBack(5);
 	ownV.print();
 	std::cout << "Element at index 2: " << ownV[2] << std::endl;
+	std::cout << "PopBack ";
 	ownV.popBack();
 	ownV.print();
+	std::cout << "Size " << ownV.size() << std::endl;
+	std::cout << "Remove on index ";
 	ownV.removeOnIndex(1);
+	ownV.print();
+	std::cout << "First index " << ownV.front() << std::endl;
+	std::cout << "Last index " << ownV.back() << std::endl;
+	ownV.clear();
 	ownV.print();
 }
