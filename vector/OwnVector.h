298648f46dd351653e3b693b2e43fc9c6cd1a41b
Jonáš <jonas.hlinak16@gmail.com>
2026-06-16 09:21:24 +0200

    Update OwnVector.h

--- vector/OwnVector.h
+++ vector/OwnVector.h
@@ -7,8 +7,12 @@ public:
 	void pushBack(int number);
 	void popBack();
 	void print();
-	void removeOnIndex(int index);
 	int operator[](int index);
+	int size();
+	void removeOnIndex(int index);
+	int front();
+	int back();
+	void clear();
 private:
 	int* mArray;
 	int mSize;
